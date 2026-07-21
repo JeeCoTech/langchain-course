eca92aab0a8ab5c6da0def9b35ec8e3c551c3467
Jeeson Colaco Tech <jeesoncolacotech@gmail.com>
2026-07-21 10:06:39 +0530

    Added changes for structured output_pydantic

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from dotenv import load_dotenv
+from typing import List
+
+from pydantic import BaseModel, Field
+
 load_dotenv()
 
 from langchain.agents import create_agent
@@ -7,14 +11,29 @@ from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
+class Source(BaseModel):
+    """Schema for a source used by the agent."""
+
+    url:str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for the agent's response."""
+
+    answer: str = Field(description="The answer to the user's query")
+    sources: List[Source] = Field(default_factory=list, description="List of sources used to generate the answer")   
 
 llm = ChatOpenAI(model="gpt-5")
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
+
 
 def main():
     print("Hello from langchain-course!")
-    result = agent.invoke({"messages":HumanMessage(content="What is the weather in Tokyo?")})
+    result = agent.invoke(
+        {"messages": HumanMessage(content="What is the weather in Tokyo?")}
+    )
     print(result)
+
+
 if __name__ == "__main__":
     main()
